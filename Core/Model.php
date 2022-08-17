a97fc930306813626b7fb0fda50984e8498a41df
aleksandr-markov <83805281+aleksandr-markov@users.noreply.github.com>
2022-08-17 12:21:51 +0300

    Update Model.php

--- Core/Model.php
+++ Core/Model.php
@@ -7,13 +7,11 @@ use PDO;
 
 class Model
 {
-//    protected $dataConnect;
     protected $database;
 
     protected function __construct()
     {
         $this->database = new Database();
-//        $this->dataConnect = new PDO("mysql:host=localhost;dbname=blogData;charset=utf8", 'root', 'root');
     }
 
 
